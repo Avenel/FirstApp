deecf4314c7d98a1f8919e472db3d5fd27d743d4
Michael <soxx22@gmail.com>
2011-10-16 19:06:17 +0200

    Buchung added

--- app/models/buchung.rb
+++ app/models/buchung.rb
@@ -1,2 +1,7 @@
 class Buchung < ActiveRecord::Base
+
+	set_primary_keys :buchjahr, :ktonr, :bnkreis, :belegnr, :typ
+	
+	has_one :OzbKonto
+
 end
--- db/migrate/20111014085829_create_buchungs.rb
+++ db/migrate/20111014085829_create_buchungs.rb
@@ -1,8 +1,21 @@
 class CreateBuchungs < ActiveRecord::Migration
   def self.up
     create_table :buchungs do |t|
-
-      t.timestamps
+			t.integer :buchjahr, :limit => 4, :null => false
+			t.integer :ktonr, :limit => 5, :null => false
+			t.string :bnkreis, :limit => 2, :null => false
+			t.integer :belegnr, :limit => 10, :null => false
+			t.string :typ, :limit => 1, :null => false
+			t.date :belegdatum
+			t.date :buchdatum
+			t.string :buchungstext, :limit => 50, null = false
+			t.decimal :sollbetrag, :precision => 10, :scale => 2, :default => nil, :null => true
+			t.decimal :habenbetrag, :precision => 10, :scale => 2, :default => nil, :null => true
+			t.integer :sollktonr, :limit => 5, :null = false, :default => 0
+			t.integer :habenktonr, :limit => 5, :null = false, :default => 0
+			t.decimal :wsaldoacc, :precision => 10, :scale => 2, :default => 0.00, :null => false
+			t.integer :punkte, :limit => 10, :null => true, :default => nil
+			t.integer :psaldoacc, :limit => 10, :null => false, :default => 0
     end
   end
 
