d628626b6fd9db5f181b9daeb9db7663df90ff78
Martin <martin@dtp-automation.com>
2011-10-16 13:21:57 +0200

    merged

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,9 +3,19 @@ class Person < ActiveRecord::Base
    attr_accessible   :rolle, :name, :vorname, :geburtsdatum, :strasse, :hausnr, :plz, 
                      :ort, :vermerk, :email, :antragsdatum, :aufnahmedatum, :austrittsdatum
 
+   validates :email, :email => true
+
    has_many :telefon
    has_many :partner
    has_many :teilnahme
    
 
+end
+
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
 end
