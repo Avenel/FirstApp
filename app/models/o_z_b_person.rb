9055a19859741af0f5ed3c027452d532842a0d46
Michael <soxx22@gmail.com>
2011-10-19 12:34:18 +0200

    OZBPerson: mnr -> id umbenannt

--- app/models/o_z_b_person.rb
+++ app/models/o_z_b_person.rb
@@ -1,16 +1,24 @@
 class OZBPerson < ActiveRecord::Base
    
-   set_table_name "OZBPerson"
-   set_primary_key :mnr
-   attr_accessible :mnr, :ueberPnr, :passwort, :pwAendDatum, :gesperrt
+	 set_table_name "OZBPerson"
+	 set_primary_key :id
+	 
+	# Include default devise modules. Others available are:
+	# :token_authenticatable, :lockable, :timeoutable, :registerable, 
+	# :confirmable, :recoverable and :activatable
+	devise :database_authenticatable, :rememberable, :trackable, :validatable
 
-   has_one :mitglied, :foreign_key => :mnr
-   has_one :student, :foreign_key => :mnr
-   has_one :buchungonline, :foreign_key => :mnr
-   has_one :tanliste, :foreign_key => :mnr
-   has_one :gesellschafter, :foreign_key => :mnr
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :id, :password
+  # attr_accessible :id, :ueberPnr, :passwort, :pwAendDatum, :gesperrt
+
+   has_one :mitglied, :foreign_key => :id
+   has_one :student, :foreign_key => :id
+   has_one :buchungonline, :foreign_key => :id
+   has_one :tanliste, :foreign_key => :id
+   has_one :gesellschafter, :foreign_key => :id
    belongs_to :person
    
-   has_many :buergschaft, :foreign_key => :mnrG
+   has_many :buergschaft, :foreign_key => :id
 
 end
