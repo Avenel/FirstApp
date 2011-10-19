82a2479c3c94dad7481a0c334868f972070593f1
Michael <soxx22@gmail.com>
2011-10-19 12:50:36 +0200

    Devise Gem migrieren

--- db/migrate/20111019094830_add_devise_to_o_z_b_person.rb
+++ db/migrate/20111019094830_add_devise_to_o_z_b_person.rb
@@ -2,10 +2,22 @@ class AddDeviseToOZBPerson < ActiveRecord::Migration
   def self.up
   
    change_table(:OZBPerson) do |t|
+		t.remove :passwort, :pwAendDatum
+		t.database_authenticatable :null => false
+		#t.confirmable
+		#t.recoverable
+		t.rememberable
+		t.trackable
+		# t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
    end
+		
+		add_index :OZBPerson, :id,										:unique => true
+		add_index :OZBPerson, :confirmation_token,		:unique => true
+    add_index :OZBPerson, :reset_password_token,	:unique => true
   
-  end
+	end
 
   def self.down
+			
   end
 end
