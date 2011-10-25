467ec44f336a9687ea7afe4954740ea080187fc6
Avenel <rambo256@arcor.de>
2011-10-25 23:56:55 +0200

    Webseite eingedeutscht

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 OZB::Application.routes.draw do
 
-  devise_for :OZBPerson
+    devise_for :OZBPerson
   resources :OZBPerson, :id => "mnr"
   #, :controllers => { :sessions => "login" }, :path_names => { :sign_in => "login", :sign_out => "logout" }
 
