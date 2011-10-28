ce2e422449f438232caf39efec7664995b9fb7fe
Avenel <rambo256@arcor.de>
2011-10-28 19:49:42 +0200

    Suche mit Ajax, Fancybox und paginate zum testen für Buergschaften integriert

--- app/controllers/buergschaft_controller.rb
+++ app/controllers/buergschaft_controller.rb
@@ -9,6 +9,46 @@ class BuergschaftController < ApplicationController
   
   def edit
     @buergschaft = Buergschaft.find([params[:pnrB], params[:mnrG]] )
+    
+    if  params[:ktoNr].nil? then
+        @konten = OZBKonto.paginate(:page => params[:page], :per_page => 1) 
+    else
+        if params[:ktoNr].empty? and params[:mnr].empty? and  params[:name].empty? and 
+        params[:ktoEinrDatum].empty? then
+          @konten = OZBKonto.paginate(:page => params[:page], :per_page => 1) 
+        else
+          @konten = OZBKonto;
+          
+          if( !params[:ktoNr].empty? ) then
+            @konten = @konten.where(:ktoNr => params[:ktoNr])
+          end
+          
+          if( !params[:mnr].empty? ) then
+            @konten = @konten.where(:mnr => params[:mnr])
+          end
+          
+          if( !params[:name].empty? ) then
+            @personen = Person.all
+            
+            @personen.each do |person|
+              name = person.name.to_s.downcase + " " +  person.vorname.to_s.downcase
+              searchName = params[:name].downcase
+              puts name
+              if name.include? searchName then
+                @konten = @konten.where(:mnr => person.OZBPerson.first.mnr)
+                break
+              end
+              
+            end
+          end
+          
+          if( !params[:ktoEinrDatum].empty? ) then
+            @konten = @konten.where(:ktoEinrDatum => params[:ktoEinrDatum])
+          end
+          
+          @konten = @konten.paginate(:page => params[:page], :per_page => 1)
+        end
+    end
   end
     
   def save
