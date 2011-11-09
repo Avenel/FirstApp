4d719519c306f989e8329a58b1d0920d91a4947d
Martin <rambo256@arcor.de>
2011-11-09 20:48:26 +0100

    Push damit ich alles aufm Laptop hab.#

--- app/controllers/ozb_person_controller.rb
+++ app/controllers/ozb_person_controller.rb
@@ -1,6 +1,6 @@
 class OZBPersonController < ApplicationController
 
-  @@Rollen = {"Mitglied" => "M", "Fördermitglied" => "F", "Partner" => "P", "Gesellschafter" => "G", "Student" => "S"}
+  @@Rollen = {"Mitglied" => "M", "Foerdermitglied" => "F", "Partner" => "P", "Gesellschafter" => "G", "Student" => "S"}
 
   def index
 		@OZBPersonen = OZBPerson.paginate(:page => params[:page], :per_page => 1)
@@ -59,34 +59,34 @@ class OZBPersonController < ApplicationController
       @OZBPerson.save!
       @Person.save!
     rescue
-			#Person erstellen
-			@new_Person = Person.create ( :rolle => params[:rolle], :name => params[:name], :vorname => params[:vorname], :geburtsdatum => params[:gebDatum],
-																		:strasse => params[:strasse], :plz => params[:plz], :ort => params[:ort], :antragsdatum => params[:antragsdatum],
-																		:aufnahmedatum => params[:aufnahmedatum] )
-			
-			#Login erstellen
-			@new_OZBPerson = OZBPerson.create( :ueberPnr => @new_Person.pnr, :email => params[:email], :password => "ozb-2000" )
-			
-			#Kontodaten hinzufuegen
-			#@new_Bankverbindung = Bankverbindung.create ( :pnr => @new_Person.pnr, :bankKtoNr => params[:kontonr], :blz => params[:blz],
-			#																							:bankName => params[:bankname] ) 
-			
-			#Datensaetze speichern
-			@new_Person.save!
-			@new_OZBPerson.save!
-			@new_Bankverbindung.save!
-			
-			#Telefon hinzufuegen
-			if params[:telefon].length > 0
-				@new_Telefon = Telefon.create( :pnr => @new_Person.pnr, :telefonNr => params[:telefon], :telefonTyp => "Tel" )
-				@new_Telefon.save!
-			end
-			
-			#Fax hinzufuegen
-			if params[:fax].length > 0
-				@new_Fax = Telefon.create( :pnr => @new_Person.pnr, :telefonNr => params[:fax], :telefonTyp => "Fax" )
-				@new_Fax.save!
-			end				
+      #Person erstellen
+      @new_Person = Person.create( :rolle => params[:rolle], :name => params[:name], :vorname => params[:vorname], :geburtsdatum => params[:gebDatum],
+                                    :strasse => params[:strasse], :plz => params[:plz], :ort => params[:ort], :antragsdatum => params[:antragsdatum],
+                                    :aufnahmedatum => params[:aufnahmedatum] )
+
+      #Login erstellen
+      @new_OZBPerson = OZBPerson.create( :ueberPnr => @new_Person.pnr, :email => params[:email], :password => "ozb-2000" )
+
+      #Kontodaten hinzufuegen
+      #@new_Bankverbindung = Bankverbindung.create ( :pnr => @new_Person.pnr, :bankKtoNr => params[:kontonr], :blz => params[:blz],
+      #																							:bankName => params[:bankname] ) 
+
+      #Datensaetze speichern
+      @new_Person.save!
+      @new_OZBPerson.save!
+      #@new_Bankverbindung.save!
+
+      #Telefon hinzufuegen
+      if params[:telefon].length > 0
+        @new_Telefon = Telefon.create( :pnr => @new_Person.pnr, :telefonNr => params[:telefon], :telefonTyp => "Tel" )
+        @new_Telefon.save!
+      end
+
+      #Fax hinzufuegen
+      if params[:fax].length > 0
+        @new_Fax = Telefon.create( :pnr => @new_Person.pnr, :telefonNr => params[:fax], :telefonTyp => "Fax" )
+        @new_Fax.save!
+      end				
 			
 			case params[:rolle]
 			when "G"
