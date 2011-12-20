f1f493a4e9f99fbac9826a6f3e786c071d4f727f
Martin <rambo256@arcor.de>
2011-12-20 18:01:53 +0100

    Strengere Validierung einer Buergschaft

--- app/models/buergschaft.rb
+++ app/models/buergschaft.rb
@@ -11,19 +11,45 @@ class Buergschaft < ActiveRecord::Base
   
   def validate!    
     errors = ActiveModel::Errors.new(self)
+    person = nil
     
+    # Bürgschafter
     if self.pnrB == nil then
       errors.add("", "Personalnummer des Bürgschafters darf nicht leer sein.")
+    else
+      person = Person.find(self.pnrB)
+    end
+   
+    if person.nil? && self.pnrB != "" then 
+      errors.add("", "Personalnummer konnte in der Datenbank nicht gefunden werden.")
+    else
+      if person.rolle != "B" then 
+        errors.add("", "Es sind nur Bürgschafter erlaubt.")
+      end
     end
     
+    # Gesellschafter
+    person = nil
     if self.mnrG == nil then
       errors.add("", "Mitgliedsnummer des Gesellschafters darf nicht leer sein.") 
+    else
+      person = Person.find(self.mnrG)
     end
     
+    if person.nil? && self.mnrG != "" then 
+      errors.add("", "Personalnummer konnte in der Datenbank nicht gefunden werden.")
+    else
+      if person.rolle != "G" then 
+        errors.add("", "Es sind nur Gesellschafter erlaubt.")
+      end
+    end
+    
+    # Kontonummer
     if self.ktoNr == nil then
       errors.add("", "Die Kontonummer darf nicht leer sein.")
     end
     
+    # Sicherheitsbetrag
     if self.sichBetrag.nil? then 
       errors.add("", "Bitte geben Sie einen Sicherheitsbetrag größer 0 an.")
     end
