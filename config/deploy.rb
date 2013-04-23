5ca36fef9d5f91a1d92a00c53e8ac2c9c3678cfe
Avenel <martin.briewig@googlemail.com>
2013-04-23 15:42:26 +0200

    added tmp/cache to gitignore path

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :application, "ozbapp"
 server "188.64.45.50", :web, :app, :db, :primary => true
 
 set :scm, "git"
-set :repository, "git://github.com/Avenel/FirstApp.git"
+set :repository, "https://github.com/Avenel/FirstApp.git"
 set :branch, "master"
 
 set :user, "ozbapp"
@@ -15,16 +15,16 @@ ssh_options[:forward_agent] = true
 
 namespace :deploy do
 
-  desc "Tell Passenger to restart the app."
-  task :restart do
-    run "touch #{current_path}/ozbapp/tmp/restart.txt"
-  end
+	desc "Tell Passenger to restart the app."
+	task :restart do
+		run "touch #{current_path}/ozbapp/tmp/restart.txt"
+	end
 
-  desc "Renew SymLink"
-  task :renew_symlink do
-  	run "rm -rf /home/ozbapp/ozbapp"
-  	run "ln -s /home/ozbapp/apps/ozbapp/current/ozbapp /home/ozbapp/ozbapp"
-  end
+	desc "Renew SymLink"
+	task :renew_symlink do
+		run "rm -rf /home/ozbapp/ozbapp"
+		run "ln -s /home/ozbapp/apps/ozbapp/current/ozbapp /home/ozbapp/ozbapp"
+end
 
 end
 
