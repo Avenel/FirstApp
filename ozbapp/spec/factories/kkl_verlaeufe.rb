a7bd2ef8920bbfa06e15cbe2612b58ab7690ffec
soXx <soxx22@gmail.com>
2013-06-10 17:18:10 +0200

    Test: kkl_verlauf

--- ozbapp/spec/factories/kkl_verlaeufe.rb
+++ ozbapp/spec/factories/kkl_verlaeufe.rb
@@ -3,9 +3,18 @@ require 'faker'
 FactoryGirl.define do
 
 	factory :KklVerlauf do
-		sequence(:ktoNr) {|n| "#{n}"} 
+		sequence(:KtoNr) {|n| "#{n}"} 
 		KKLAbDatum = Date.today
 		sequence(:KKL) {|n| "#{n}"}
+
+		factory :kklverlauf_with_ozbkonto_with_kontenklasse do
+			before(:create) do |kklverlauf|
+				ozbkonto = FactoryGirl.create(:ozbkonto_with_ozbperson)
+				kontenklasse = FactoryGirl.create(:kontenklasse_A)
+				kklverlauf.ktoNr = ozbkonto.ktoNr
+				kklverlauf.kkl = kontenklasse.kkl
+			end
+		end
 	end
 
 end
--- ozbapp/spec/models/kkl_verlauf_spec.rb
+++ ozbapp/spec/models/kkl_verlauf_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe KklVerlauf do
+	#Factory
+	it "has a valid factory" do
+		expect(FactoryGirl.create(:kklverlauf_with_ozbkonto_with_kontenklasse)).to be_valid
+	end
 
 end
