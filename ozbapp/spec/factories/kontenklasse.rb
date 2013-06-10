4943561aaf129a676e056e203f7dadc355f01993
soXx <soxx22@gmail.com>
2013-06-10 14:06:28 +0200

    Test: Kontenklasse

--- ozbapp/spec/factories/kontenklasse.rb
+++ ozbapp/spec/factories/kontenklasse.rb
@@ -3,6 +3,28 @@ require 'faker'
 FactoryGirl.define do
 
 	factory :Kontenklasse do
-	end
+		KKL "0"
+		KKLAbDatum Date.today
+		Prozent 0.00
 
+		factory :kontenklasse_A do
+			KKL "1"
+			Prozent 100.00
+		end
+
+		factory :kontenklasse_B do
+			KKL "2"
+			Prozent 75.00
+		end
+
+		factory :kontenklasse_C do
+			KKL "3"
+			Prozent 50.00
+		end
+
+		factory :kontenklasse_D do
+			KKL "4"
+			Prozent 25.00
+		end
+	end
 end
