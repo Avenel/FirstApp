8419f512fdf49cf262b2715dce6efa43df9829a2
Avenel <martin.briewig@googlemail.com>
2013-06-11 10:26:19 +0200

    added last tests for projektgruppe, changed validation

--- ozbapp/app/models/projektgruppe.rb
+++ ozbapp/app/models/projektgruppe.rb
@@ -8,6 +8,7 @@ class Projektgruppe < ActiveRecord::Base
   
   	# aliases
   	alias_attribute :pgNr, :Pgnr
+    alias_attribute :projGruppenBez, :ProjGruppenBez
 
   	# attributes
   	attr_accessible :pgNr, :Pgnr, :ProjGruppenBez
@@ -16,6 +17,28 @@ class Projektgruppe < ActiveRecord::Base
   	has_many :ZEKonto, :inverse_of => :Projektgruppe, :foreign_key => :pgNr # Done, getestet
 
   	# validations
-  	validates :pgNr, :presence => { :format => { :with => /^([0-9]+)$/i }, :message => "Bitte geben Sie eine gültige Projektgruppennummer an." }
+  	validates :pgNr, :presence => true 
+
+    validate :valid_pgNr
+
+    def valid_pgNr 
+      # every projektgruppennummer is > 0
+      # if a string is passed to pgNr, it equals 0 => false
+      if self.pgNr == 0 then
+        errors.add :Pgnr, "projektgruppennummer ist nicht valide."
+        return false
+      else 
+        # already exists?
+        pg = Projektgruppe.where("Pgnr = ?", self.pgNr).first
+
+        if pg.nil? then
+          return true
+        else
+          errors.add :Pgnr, "projektgruppennummer bereits vergeben."
+          return false
+        end
+      end
+
+    end
 
 end
--- ozbapp/spec/models/projektgruppe_spec.rb
+++ ozbapp/spec/models/projektgruppe_spec.rb
@@ -4,12 +4,17 @@ describe :Projektgruppe do
 
 	# Factory
 	it "has a valid factory" do
-		expect(FactoryGirl.create(:Projektgruppe))
+		projektgruppe = FactoryGirl.build(:Projektgruppe)
+		expect(projektgruppe).to be_valid
+		expect(projektgruppe.save).to eq true
 	end
 
 	# Valid/invalid attributes
+	# pgNr = Projektgruppennummer
 	it "is valid with a valid Projektgruppennummer" do
-		expect(FactoryGirl.create(:Projektgruppe, :pgNr => 42)).to be_valid
+		projektgruppe = FactoryGirl.build(:Projektgruppe, :pgNr => 42)
+		expect(projektgruppe).to be_valid
+		expect(projektgruppe.save).to eq true
 	end
 
 	it "is invalid without a Projektgruppennummer" do
@@ -20,7 +25,57 @@ describe :Projektgruppe do
 		expect(FactoryGirl.build(:Projektgruppe, :pgNr => "a1234")).to be_invalid
 	end
 
+	# ProjGruppenBez
+	it "is valid with a valid GruppenBezeichnung" do
+		projektgruppe = FactoryGirl.build(:Projektgruppe, :projGruppenBez => "Mustergruppe")
+		expect(projektgruppe).to be_valid
+		expect(projektgruppe.save).to eq true
+	end
+
+	it "is valid without a GruppenBezeichnung" do
+		projektgruppe = FactoryGirl.build(:Projektgruppe, :projGruppenBez => nil)
+		expect(projektgruppe).to be_valid
+		expect(projektgruppe.save).to eq true
+	end
 
 	# Class and instance methods
+	# valid_pgNr
+	it "returns true, if pgNr is greater 0, not a string and there is no record with the same pgNr" do
+		projektgruppe = FactoryGirl.build(:Projektgruppe)
+		projektgruppe.pgNr = 42
+
+		expect(projektgruppe.valid_pgNr).to eq true
+		expect(projektgruppe.save).to eq true
+	end	
+
+	it "returns false, if pgNr is a string" do
+		projektgruppe = FactoryGirl.build(:Projektgruppe)
+		projektgruppe.pgNr = "helloWorld"
+
+		expect(projektgruppe.valid_pgNr).to eq false
+		expect(projektgruppe.save).to eq false
+	end
+
+	it "returns false, if pgNr equals 0" do
+		projektgruppe = FactoryGirl.build(:Projektgruppe)
+		projektgruppe.pgNr = 0
+
+		expect(projektgruppe.valid_pgNr).to eq false
+		expect(projektgruppe.save).to eq false
+	end
+
+	it "returns false, if pgNr already exists in the database" do
+		projektgruppe = FactoryGirl.build(:Projektgruppe)
+		projektgruppe.pgNr = 45
+
+		expect(projektgruppe.valid_pgNr).to eq true
+		expect(projektgruppe.save).to eq true
+
+		projektgruppe = FactoryGirl.build(:Projektgruppe)
+		projektgruppe.pgNr = 45
+
+		expect(projektgruppe.valid_pgNr).to eq false
+		expect(projektgruppe.save).to eq false
+	end
 
 end
