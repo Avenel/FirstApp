b85e2937d8bbe5c02a452b920aca96a56ab622e2
soXx <soxx22@gmail.com>
2013-06-12 15:57:26 +0200

    Added validations to Buchung

--- ozbapp/app/models/buchung.rb
+++ ozbapp/app/models/buchung.rb
@@ -10,24 +10,106 @@ class Buchung < ActiveRecord::Base
                   :HabenKtoNr, :WSaldoAcc, :Punkte, :PSaldoAcc
 
   # associations
-  belongs_to :OZBKonto, :foreign_key => :KtoNr
+  belongs_to :OZBKonto, 
+    :foreign_key => :KtoNr, 
+    :class_name => "OzbKonto"
   
   # validations
-  # ...
-  
+  validates :BuchJahr, 
+    :presence {
+      :message => "Bitte geben Sie ein {HUMANIZED_ATTRIBUTES[:BuchJahr]} an."},
+    :length { :is => 4, 
+      :message => "Das {HUMANIZED_ATTRIBUTES[:BuchJahr]} muss 4-stellig sein."},
+    :numericality => { :only_integer => true, 
+      :message => "Das Jahr darf nur aus Ziffern bestehen." }
+
+  validates :KtoNr, 
+    :presence => { :format => { :with => /^[0-9]{5}$/i  },
+      :message => "Bitte geben Sie eine Konto-Nr. an." }, 
+    :length => { :is => 5, :message => "Die Konto-Nr. darf nur 5-stellig sein." }, 
+    :numericality => { :only_integer => true, 
+      :message => "Die Konto-Nr. darf nur Zahlen beinhalten." }
+
+  #Nicht spezifiziert (außer DB-Einschraenkung)
+  validates :BnKreis, 
+    :presence => {:message => "Bitte geben Sie einen Belegnummernkreis an." }
+
+  #Nicht spezifiziert (außer DB-Einschraenkung)
+  validates :BelegNr, 
+    :presence => {:message => "Bitte geben Sie eine Belegnummer an." }
+
+  #Nicht spezifiziert (außer DB-Einschraenkung)
+  validates :Typ, 
+    :presence => {:message => "Bitte geben Sie einen Typ an." }
+
+  #Nicht spezifiziert (außer DB-Einschraenkung)
+  validates_date :Belegdatum, 
+    :presence => {:message => "Bitte geben Sie einen Belegdatum an." }
+
+  #Nicht spezifiziert (außer DB-Einschraenkung)
+  validates_date :BuchDatum, 
+    :presence => {:message => "Bitte geben Sie einen Buchungsdatum an." }
+
+  #Nicht spezifiziert (außer DB-Einschraenkung)
+  validates :BuchungsText, 
+    :presence => {:message => "Bitte geben Sie einen Verwendungszweck an." },
+    :length => {:maximum => 50, 
+      :message => "Der Verwendungszweck ist zu lang. Maximal 50 Zeichen erlaubt."}
+
+  #Nicht spezifiziert (außer DB-Einschraenkung)
+  validates :SollBetrag, 
+    :presence => {:message => "Bitte geben Sie einen Betrag (Soll) an." }, 
+    :format => { :with => /^\d{,10}[.]?\d{0,2}$/, 
+      :message => "Bitte geben Sie einen gültigen Betrag an." }
+
+  #Nicht spezifiziert (außer DB-Einschraenkung)
+  validates :HabenBetrag, 
+    :presence => {:message => "Bitte geben Sie einen Betrag (Haben) an." }, 
+    :format => { :with => /^\d{,10}[.]?\d{0,2}$/, 
+      :message => "Bitte geben Sie einen gültigen Betrag an." }
+
+  validates :SollKtoNr, 
+    :presence => { :format => { :with => /^[0-9]{5}$/i  },
+      :message => "Bitte geben Sie eine Konto-Nr. (Soll) an." }, 
+    :length => { :is => 5, :message => "Die Konto-Nr. (Soll) darf nur 5-stellig sein." }, 
+    :numericality => { :only_integer => true, 
+      :message => "Die Konto-Nr. (Soll) darf nur Zahlen beinhalten." }
+
+  validates :HabenKtoNr, 
+    :presence => { :format => { :with => /^[0-9]{5}$/i  },
+      :message => "Bitte geben Sie eine Konto-Nr. (Haben) an." }, 
+    :length => { :is => 5, :message => "Die Konto-Nr. (Haben) darf nur 5-stellig sein." }, 
+    :numericality => { :only_integer => true, 
+      :message => "Die Konto-Nr. (Haben) darf nur Zahlen beinhalten." }
+
+  #Nicht spezifiziert (außer DB-Einschraenkung)
+  validates :WSaldoAcc, 
+    :presence => {:message => "Bitte geben Sie einen wSaldoAcc an." }, 
+    :format => { :with => /^\d{,10}[.]?\d{0,2}$/, 
+      :message => "Bitte geben Sie einen gültigen wSaldoAcc an." }
+
+  #Nicht spezifiziert (außer DB-Einschraenkung)
+  validates :Punkte, 
+    :presence => {:message => "Bitte geben Sie die Punkte an." }
+
+  #Nicht spezifiziert (außer DB-Einschraenkung)
+  validates :PSaldoAcc, 
+    :presence => {:message => "Bitte geben Sie die pSaldoAcc an." }
+    
   # column names
   HUMANIZED_ATTRIBUTES = {
     :BuchJahr       => 'Jahr',
     :KtoNr          => 'Konto-Nr.',
-    :BnKreis        => 'Buchungskreis',
+    :BnKreis        => 'Belegnummernkreis',
+    :BelegNr        => 'Belegnummer',
     :Typ            => 'Typ',
     :Belegdatum     => 'Belegdatum',
     :BuchDatum      => 'Buchungsdatum',
     :BuchungsText   => 'Verwendungszweck',
     :SollBetrag     => 'Betrag (Soll)',
     :HabenBetrag    => 'Betrag (Haben)',
-    :SollKtoNr      => 'sollKtoNr',
-    :HabenKtoNr     => 'habenKtoNr',
+    :SollKtoNr      => 'Konto (Soll)',
+    :HabenKtoNr     => 'Konto (Haben)',
     :WSaldoAcc      => 'wSaldoAcc',
     :Punkte         => 'Punkte',
     :PSaldoAcc      => 'pSaldoAcc'
