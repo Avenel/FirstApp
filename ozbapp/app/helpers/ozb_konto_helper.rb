1ff3c7d142fe616b83e4dd7fd4b748c51a273ba1
avenel <martin.briewig@googlemail.com>
2013-09-04 19:26:07 +0200

    fixed some typos.

--- ozbapp/app/helpers/ozb_konto_helper.rb
+++ ozbapp/app/helpers/ozb_konto_helper.rb
@@ -3,23 +3,23 @@ module OzbKontoHelper
   # builds the relation objects if they're not already assigned via mass-assignment
   def setup_ee_konto(ozb_konto)
     # create new KKLVerlauf to get fields
-    if (ozb_konto.kkl_verlauf.nil?)
+    if (ozb_konto.KklVerlauf.nil?)
        ozb_konto.build_kkl_verlauf # has_one
     end
     
     # create new EEKonto to get fields
-    if (ozb_konto.ee_konto.nil?)
+    if (ozb_konto.EeKonto.nil?)
       ozb_konto.build_ee_konto # has_one
     end
     
     # create new Bankverbindung to get fields
-    if (ozb_konto.ee_konto.Bankverbindung.nil?)
+    if (ozb_konto.EeKonto.Bankverbindung.nil?)
       ozb_konto.ee_konto.build_Bankverbindung # belongs_to
     end
     
     # create new Bank to get fields
-    if (ozb_konto.ee_konto.Bankverbindung.Bank.nil?)
-      ozb_konto.ee_konto.Bankverbindung.build_Bank # belongs_to
+    if (ozb_konto.EeKonto.Bankverbindung.Bank.nil?)
+      ozb_konto.EeKonto.Bankverbindung.build_Bank # belongs_to
     end
     
     return ozb_konto
@@ -28,12 +28,12 @@ module OzbKontoHelper
   # builds the relation objects if they're not already assigned via mass-assignment
   def setup_ze_konto(ozb_konto)
     # create new KKLVerlauf to get fields
-    if (ozb_konto.kkl_verlauf.nil?)
+    if (ozb_konto.KklVerlauf.nil?)
        ozb_konto.build_kkl_verlauf # has_one
     end
     
     # create new EEKonto to get fields
-    if (ozb_konto.ze_konto.nil?)
+    if (ozb_konto.ZeKonto.nil?)
       ozb_konto.build_ze_konto # has_one
     end
     
