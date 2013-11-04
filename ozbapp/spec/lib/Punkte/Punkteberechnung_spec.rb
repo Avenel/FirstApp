3496029406ac2e7b48714eedcb23ca044e433428
mail@thomaseger.de <mail@thomaseger.de>
2013-11-04 09:46:45 +0100

    Formatted Punkteberechnung_spec.rb.

--- ozbapp/spec/lib/Punkte/Punkteberechnung_spec.rb
+++ ozbapp/spec/lib/Punkte/Punkteberechnung_spec.rb
@@ -32,7 +32,7 @@ describe Punkteberechnung, "#get_days_in_account_classes()" do
     date_begin = "2008-07-15".to_time
     date_end = "2008-08-05".to_time
     account_number = 70013
-	account_classes = Punkteberechnung::get_affected_account_classes(date_begin, date_end, account_number)
+    account_classes = Punkteberechnung::get_affected_account_classes(date_begin, date_end, account_number)
     
     days_in_account_classes = Punkteberechnung::get_days_in_account_classes(account_classes, date_begin, date_end)
     
@@ -40,17 +40,15 @@ describe Punkteberechnung, "#get_days_in_account_classes()" do
     expect(days_in_account_classes["C"]).to eq 4
   end 
 
-  it "works for a longer period with more than 2 account classes" do
-  	#todo
-    expect(5).to eq 4
-  end 
+  it "works for a longer period with more than 2 account classes"
+
 end
 
 describe Punkteberechnung, "#get_factor_for_account_class()" do
 	it "returns 0.75 for class B" do
 		date_begin = "2008-07-15".to_time
-	    date_end = "2008-08-05".to_time
-	    account_number = 70013
+    date_end = "2008-08-05".to_time
+	  account_number = 70013
 		account_classes = Punkteberechnung::get_affected_account_classes(date_begin, date_end, account_number)
 		
 		factor = Punkteberechnung::get_factor_for_account_class(account_classes[0]).to_f
