e03a5d43aa071dfc984d9fb3e07fcc173635df73
mail@thomaseger.de <mail@thomaseger.de>
2013-11-16 11:58:44 +0100

    Refactoring of specs to match the others.

--- ozbapp/spec/lib/Punkte/Punkteberechnung_spec.rb
+++ ozbapp/spec/lib/Punkte/Punkteberechnung_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 require 'Punkteberechnung'
 
-describe Punkteberechnung, "#calculate()" do
-  it "returns 483" do
+describe Punkteberechnung do
+  it "calculates a score of 483" do
     date_begin = "2008-07-15".to_time
     date_end = "2008-08-05".to_time
     amount = 1000
@@ -12,9 +12,7 @@ describe Punkteberechnung, "#calculate()" do
     
     expect(points).to eq 483
   end 
-end
 
-describe Punkteberechnung, "#get_affected_account_classes()" do
   it "returns B and C as affected account classes" do
     date_begin = "2008-07-15".to_time
     date_end = "2008-08-05".to_time
@@ -25,10 +23,8 @@ describe Punkteberechnung, "#get_affected_account_classes()" do
     expect(account_classes[0].KKL).to eq 'B'
     expect(account_classes[1].KKL).to eq 'C'
   end 
-end
 
-describe Punkteberechnung, "#get_days_in_account_classes()" do
-  it "returns 16 for class B and 5 for class C" do
+  it "returns 16 days for class B and 5 days for class C" do
     date_begin = "2008-07-15".to_time
     date_end = "2008-08-05".to_time
     account_number = 70013
@@ -40,12 +36,9 @@ describe Punkteberechnung, "#get_days_in_account_classes()" do
     expect(days_in_account_classes["C"]).to eq 5
   end 
 
-  it "works for a longer period with more than 2 account classes"
+  it "calculates the factor 1.0 for class A" 
 
-end
-
-describe Punkteberechnung, "#get_factor_for_account_class()" do
-	it "returns 0.75 for class B" do
+	it "calculates the factor 0.75 for class B" do
 		date_begin = "2008-07-15".to_time
     date_end = "2008-08-05".to_time
 	  account_number = 70013
@@ -55,10 +48,14 @@ describe Punkteberechnung, "#get_factor_for_account_class()" do
 
 		expect(factor).to eq 0.75
 	end
-end 
 
-describe Punkteberechnung, "#count_days_exact()" do
-  it "returns 16" do
+  it "calculates the factor 0.5 for class C"
+
+  it "calculates the factor 0.25 for class D"
+  
+  it "calculates the factor 0.0 for class E"
+
+  it "returns 16 days" do
     date_begin = "2008-07-15".to_time
     date_end = "2008-08-01".to_time
     
@@ -67,7 +64,7 @@ describe Punkteberechnung, "#count_days_exact()" do
     expect(days).to eq 16
   end
 
-  it "returns 5" do
+  it "returns 5 days" do
     date_begin = "2008-08-01".to_time
     date_end = "2008-08-05".to_time
     
@@ -76,7 +73,7 @@ describe Punkteberechnung, "#count_days_exact()" do
     expect(days).to eq 5
   end
 
-  it "returns 31" do
+  it "returns 31 days" do
     date_begin = "2013-01-01".to_time
     date_end = "2013-02-01".to_time
     
@@ -84,5 +81,8 @@ describe Punkteberechnung, "#count_days_exact()" do
 
     expect(days).to eq 31
   end
-end
 
+  it "works for a longer period with more than 2 account classes"
+
+  it "works for more than one booking in one day"
+end
